36521a9f151bc73f96d9f97b8ca6eee2c44a02ce
Darusia <61788990+darkchiii@users.noreply.github.com>
2025-09-18 14:27:42 +0200

    Add error handling, cleaned code

--- data_loader.py
+++ data_loader.py
@@ -11,28 +11,35 @@ scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-
-creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
-client = gspread.authorize(creds)
 load_dotenv()
 
 
 def load_data():
     try:
-        spreadheet_key = Config.SPREADSHEET_KEY
+        creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+        print("Credentials loaded")
+        client = gspread.authorize(creds)
+        print("Client authorized")
+        try:
+            spreadheet_key = Config.SPREADSHEET_KEY2
+        except AttributeError:
+            raise ValueError("Spreadsheet key not defined in file: config.py. Please set SPREADSHEET_KEY.")
 
         if not spreadheet_key:
-            raise ValueError("SPREADSHEET_KEY not found in environment variables.")
-
-
+            raise ValueError("SPREADSHEET_KEY is empty. Please set it in environment variables or config.py.")
         sheet = client.open_by_key(spreadheet_key).sheet1
+        print("Spreadsheet opened")
         data = sheet.get_all_records()
+        print(f"Got {len(data)} records")
         df = pd.DataFrame(data)
 
         if df.empty:
             raise ValueError("No data in spreadsheet.")
 
         return df, sheet
+    except FileNotFoundError as e:
+        print(f"Credentials file - credentials.json not found: {e}")
+        raise
     except Exception as e:
         print(f"Error while loading data: {e}")
         raise
